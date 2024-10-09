648ef83740f7db73277e0892a35c0545f8049741
Priyangshu <priyangshuchakraborty.25cs@licet.ac.in>
2024-10-09 14:26:38 +0530

    v09103

---
+++ src/main/java/io/reflectoring/farm/entity/User.java
@@ -0,0 +1,27 @@
+package io.reflectoring.farm.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import jakarta.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+}
---
+++ src/main/java/io/reflectoring/farm/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package io.reflectoring.farm.repository;
+
+public class UserRepository {
+
+}
